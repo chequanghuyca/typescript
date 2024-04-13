c94be525213b8a1cf0c3a54fb1cf96fc5d959144
Huy Chè <chequanghuybtt@gmail.com>
2024-04-14 06:27:38 +0700

    break

---
+++ control-flow/chapter6 - break/chapter6.ts
@@ -0,0 +1,11 @@
+// Sử dụng break để kết thúc vòng lặp
+// Chúng ta có thể sử dụng break để kết thúc vòng lặp for, while, do while hoặc switch case.
+// Ví dụ:
+let a: number = 0;
+while (a < 5) {
+    console.log(i);
+    a++;
+    if (a === 3) {
+        break;
+    }
+}
