24eeecc8d34b041eb9439b14242f13da329e81f8
Huy Chè <chequanghuybtt@gmail.com>
2024-04-14 06:15:59 +0700

    if else

---
+++ control-flow/chapter1 - if else/chapter1.ts
@@ -0,0 +1,15 @@
+// Tương tự như JS, if else trong TS cũng hoạt động tương tự.
+
+// if (condition) {
+//     // code block
+// } else {
+//     // code block
+// }
+
+// Ví dụ:
+let isShow: boolean = true;
+if (isShow) {
+    console.log('Show');
+} else {
+    console.log('Hide');
+}
