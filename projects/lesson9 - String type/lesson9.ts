e1a54094aa77b6ea3e604a814396886c647ed0f9
Huy Chè <chequanghuybtt@gmail.com>
2024-04-14 03:16:58 +0700

    lesson9 String type

---
+++ projects/lesson9 - String type/lesson9.ts
@@ -0,0 +1,10 @@
+let name2: string = ` và "Che Quang Huy"`;
+let name3 = `Huy ${name2}`; //template strings js
+
+console.log(">>> check name = ", name3)
+
+
+// Tương tự Javascript, chúng ta có thể dùng double quotes (") hoặc single quotes (') để khai báo string.
+// let firstName: string = 'ABC';
+// let fullName: string;
+// fullName = "DEF";
