0cfe2c676fb6ddee15e952f5a64cdd8a728d0d7f
Huy Chè <chequanghuybtt@gmail.com>
2024-04-14 06:21:46 +0700

    while

---
+++ control-flow/chapter4 - while/chapter4.ts
@@ -0,0 +1,9 @@
+// While trong TypeScript cũng hoạt động tương tự như trong JavaScript.
+// Giống như for loop, while loop cũng được sử dụng để lặp qua một khối mã nếu điều kiện được cung cấp là true.
+// Nếu như với for loop, chúng ta biết được số lần lặp cụ thể, thì với while loop, chúng ta không biết trước số lần lặp.
+// Ví dụ:
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
