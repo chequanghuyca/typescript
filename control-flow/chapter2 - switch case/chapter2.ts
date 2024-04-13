8a458cb2a54d3b6fb39d7ae88d1cdc419740b9eb
Huy Chè <chequanghuybtt@gmail.com>
2024-04-14 06:17:39 +0700

    switch case

---
+++ control-flow/chapter2 - switch case/chapter2.ts
@@ -0,0 +1,14 @@
+// Switch case trong TypeScript cũng hoạt động tương tự như trong JavaScript.
+
+// Ví dụ:
+let color: string = 'red';
+switch (color) {
+    case 'red':
+        console.log('Red');
+        break;
+    case 'blue':
+        console.log('Blue');
+        break;
+    default:
+        console.log('Color');
+}
